2d53bbd758ec065c861d3c2246723849c99fd53d
Tuomas Kanerva <tuomas@kanerva.info>
2024-08-29 14:51:59 +0300

    added docs

---
+++ client/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "todo.yaml CLI";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs_20
+              ffmpeg_5
+              sqlite
+              litecli
+              inotify-tools
+            ];
+          };
+        }
+      );
+}
