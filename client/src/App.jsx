470a810dfd5eb5e83f3536fee13db260de967395
Tuomas Kanerva <tuomas@kanerva.info>
2024-09-09 15:02:26 +0300

    updated client to new doc structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,23 +23,60 @@ function isUrlAbsolute(url) {
   return url.indexOf('#') !== 0 && idx < 10 && idx != -1
 }
 
-function useGet(url) {
-  let [request, setRequest] = useState({
-    status: 0,
-    ok: false,
-    loading: true,
-    body: null
-  })
+function formatSignature(signatureParams) {
+  return (
+    (
+      r.init(signatureParams)
+      .map(formatSignatureParam)
+      .join(', ')
+    )
+    + ' -> ' +
+    formatSignatureParam(r.last(signatureParams))
+  )
+}
 
+function formatSignatureParam(param) {
+  return `${param.name || ''}${param.name && param.type ? ':' : ''}${param.type || ''}`
+}
+
+function highlightCode({ children, className, node, ...rest }) {
+  let match = /language-(\w+)/.exec(className || '')
+  return (match)
+    ? <SyntaxHighlighter
+        {...rest}
+        className='doc-symbol__syntax-highlighter'
+        customStyle={{ backgroundColor: 'transparent' }}
+        PreTag="div"
+        data-language={ match }
+        children={ String(children).replace(/\n$/, '') }
+        language={ match[1] }
+      />
+    : <code {...rest} className={ className } data-language={ match }>
+        {children}
+      </code>
+}
+
+const DEFAULT_REQUEST = {
+  status: undefined,
+  ok: false,
+  loading: true,
+  body: null
+}
+
+function useGet(url) {
+  let [request, setRequest] = useState(DEFAULT_REQUEST)
 
   useEffect(
     () => {
+      setRequest(DEFAULT_REQUEST)
+
       fetch(url)
       .then((res) => {
-        setRequest({ ...request, status: res.status, ok: res.ok })
-        return res.text()
-      }).then((it) => {
-        setRequest({ ...request, body: it })
+        let updated = { ...request, status: res.status, ok: res.ok, loading: false }
+        setRequest(updated)
+        return res.text().then((it) => {
+          setRequest({ ...updated, body: it })
+        })
       })
     },
     [url]
@@ -49,8 +86,11 @@ function useGet(url) {
 }
 
 
-function useDocs(slug) {
-  let docs = useGet(`${REPO}/${slug}`)
+function useDocs(gen, slug) {
+  let docs = gen !== 'github'
+    ? useGet(`${REPO}/${gen}/${slug}`)
+    : useGet('https://raw.githubusercontent.com/' + slug)
+
   let [parsed, setParsed] = useState([])
 
   useEffect(() => {
@@ -62,7 +102,30 @@ function useDocs(slug) {
     }
   }, [docs.body])
 
-  return parsed
+  useEffect(() => {
+    setParsed([])
+  }, [slug])
+
+  return [parsed, docs]
+}
+
+function DataTable({ data }) {
+  return <table className="data-table">
+    <tbody>
+      {r.toPairs(data).map(([key, value]) =>
+        <tr key={ key }>
+          <th>{ key }</th>
+          <td>{
+            typeof value === 'object'
+              ? <DataTable data={ value } />
+              : key === 'signatures'
+                ? <code>{value}</code>
+                : value
+          }</td>
+        </tr>
+      )}
+    </tbody>
+  </table>
 }
 
 function DoctableSearch({ selectedDoc }) {
@@ -88,11 +151,16 @@ function DoctableSearch({ selectedDoc }) {
             it.path.toLowerCase().indexOf(query) > -1
           )
           .map((it) => ({ ...it, id: it.path, url: `/${it.path}` }))
+          .concat([{
+            id: '/something-missing',
+            url: '/something-missing'
+          }])
         }
         renderListItem={ (it) =>
-          <div className='doctable-search__result'>
+          it.path
+          ? <div className='doctable-search__result'>
             <div className='doctable-search__result-slug'>
-              { it.slug }
+              { it.path }
             </div>
             <div className='doctable-search__result-name'>
               { it.name }
@@ -102,13 +170,21 @@ function DoctableSearch({ selectedDoc }) {
               { it.version }
             </div>
           </div>
+          : <div className="doctable-search__result">
+            <div>
+              Something missing?
+            </div>
+            <div className='doctable-search__result-version'>
+              Finding more docs
+            </div>
+          </div>
         }
         />
     </div>
   </div>
 }
 
-function ListSymbols({ doc, docs, autofocus }) {
+function ListSymbols({ gen, doc, docs, autofocus }) {
   let [query, setQuery] = useState('')
   let [selected, _setSelected] = useState(0)
   let [resultsList, setResultsList] = useState()
@@ -133,10 +209,7 @@ function ListSymbols({ doc, docs, autofocus }) {
   }, [searchInput])
 
   let docsWithIds = docs.map((it, idx) =>
-    ({ ...it, id: idx, url: `/${doc}/${idx}` })
-  ).filter((it) =>
-    (it.summary || '') != '' ||
-    (it.description || '') != ''
+    ({ ...it, id: idx, url: `/${gen}/${encodeURIComponent(doc)}/${idx}` })
   )
 
   let searchRanking = (it='') => {
@@ -154,7 +227,6 @@ function ListSymbols({ doc, docs, autofocus }) {
   return <div className='doc-search'>
     <SearchDropdown
       ref={ setSearchInput }
-
       getQueriedItems={ (query) => {
         return r.take(100, query === ''
           ? docsWithIds
@@ -174,12 +246,18 @@ function ListSymbols({ doc, docs, autofocus }) {
 
       }}
       renderListItem={ (it) => <>
+        <div className="doc-search__result-kind">
+          { it.kind }
+        </div>
         <div className="doc-search__result-name">
-          { it.name }
+          <code className='doc-search__result-ns'>{ it.ns }</code> { it.name }
         </div>
         <div className="doc-search__result-summary">
           { it.summary }
         </div>
+        { it.signatures && <code className="doc-search__result-signatures">
+          { formatSignature(it.signatures) }
+        </code> }
       </>}
       placeholder='🔎  Search for docs... (Type /)'/>
   </div>
@@ -188,37 +266,86 @@ function ListSymbols({ doc, docs, autofocus }) {
 function CopyrightNotice({ headers }) {
   return <div className="doc-copyright">
     { headers.name } documentation {'\n'}
-    { headers.copyright || `licensed under ${headers.license}` }
+    { headers.copyright || `license: ${headers.license}` }
   </div>
 }
 
 function Loader() {
   return <div className="loader__wrapper">
+    <div className="loader__notice">
+      Parsing docs. <br/>This can take some time...
+    </div>
     <div className="loader"></div>
   </div>
 }
 
-function DocPage({ doc, symbolId }) {
-  let [headers, docs, urlTable] = useDocs(doc)
+function preformatSymbol(it) {
+  return r.pipe(
+    r.omit(['description', 'ns', 'signatures']),
+    it.signatures
+      ? r.mergeLeft({
+        signatures: formatSignature(it.signatures)
+      })
+      : r.identity,
+    it.ns
+      ? r.mergeRight({
+        namespace: it.ns
+      })
+      : r.identity
+  )(it)
+}
+
+function DocPage({ gen, doc, symbolId }) {
+  let [[headers, docs, urlTable], request] = useDocs(gen, doc)
 
   useEffect(() => {
-    document.title = `pkDocs - ${doc}`
-  }, [doc])
+    if (headers && headers.name) {
+      document.title = `pkDocs - ${doc}`
+    }
+  }, [headers && headers.name])
 
-  if (!docs) {
+  if (request.loading || !docs) {
     return <Loader />
   }
 
-  let symbolSearch = <ListSymbols autofocus={ symbolId === undefined } doc={ doc } docs={ docs } />
+  if (request.status === 404) {
+    return <NotFound />
+  }
+
+  let symbolSearch = <ListSymbols autofocus={ symbolId === undefined } gen={ gen } doc={ doc } docs={ docs } />
+
+  let markdownComponents = {
+    a(props) {
+      if (isUrlAbsolute(props.href || '')) {
+        return <a {...props} />
+      } else if (props.href.indexOf('#') === 0) {
+        return props.children
+      } else if (urlTable[decodeURIComponent(props.href)] !== undefined) {
+        return <Link {...props} href={ `/${gen}/${encodeURIComponent(doc)}/${urlTable[decodeURIComponent(props.href)]}` } />
+      } else {
+        return props.children
+      }
+    },
+
+    code: highlightCode
+  }
 
   if (!symbolId) {
     return <div>
       {symbolSearch}
 
       { headers.description &&
-        <div className="doc-symbol">
-          <h1>{ headers.name } { headers.version }</h1>
-          <pre>{ headers.description }</pre>
+        <div className="doc-frontpage">
+          <div className="doc-symbol">
+            <h1>{ headers.name } { headers.version }</h1>
+            <div className="doc-symbol__body">
+              <Markdown
+                components={ markdownComponents }
+                remarkPlugins={[remarkGfm]}>
+                  { headers.description }
+              </Markdown>
+            </div>
+          </div>
         </div>
       }
 
@@ -232,50 +359,55 @@ function DocPage({ doc, symbolId }) {
       { symbolSearch }
       <div className="doc-symbol">
         <h1>{symbol.name}</h1>
-        <div className="doc-symbol__body">
-          <Markdown
-            components={{
-              a(props) {
-                if (isUrlAbsolute(props.href || '')) {
-                  return <a {...props} />
-                } else if (props.href.indexOf('#') === 0) {
-                  return props.children
-                } else if (urlTable[props.href] !== undefined) {
-                  return <Link {...props} href={ `/${doc}/${urlTable[props.href]}` } />
-                } else {
-                  return props.children
-                }
-              },
+        <DataTable data={ preformatSymbol(symbol) } />
 
-              code({ children, className, node, ...rest }) {
-                let match = /language-(\w+)/.exec(className || '')
-                return (match)
-                  ? <SyntaxHighlighter
-                      {...rest}
-                      className='doc-symbol__syntax-highlighter'
-                      customStyle={{ backgroundColor: 'transparent' }}
-                      PreTag="div"
-                      data-language={ match }
-                      children={ String(children).replace(/\n$/, '') }
-                      language={ match[1] }
-                    />
-                  : <code {...rest} className={ className } data-language={ match }>
-                      {children}
-                    </code>
-              }
-            }}
-            remarkPlugins={[remarkGfm]}>{symbol.description}</Markdown>
-        </div>
-
+        { headers.text_format === 'markdown'
+          ? <div className="doc-symbol__body">
+            <Markdown
+              components={ markdownComponents }
+              remarkPlugins={[remarkGfm]}>
+                {symbol.description}
+            </Markdown>
+          </div>
+          : <pre>{ symbol.description }</pre> }
         <CopyrightNotice headers={ headers } />
       </div>
     </div>
   }
 }
 
-function FrontPage() {
-  return <div>
+function SomethingMissing() {
+  return <div className="something-missing"><div className="doc-symbol">
+    <div className="doc-symbol__body">
+      <Markdown>{`
+# Something missing?
 
+The documentation on pkDocs is selected from the [pikadoc central repository](https://github.com/ArseAssassin/pkdocs/tree/main/docs), a library containing reference documentation for 150+ modern languages/frameworks/libraries. While we'd love to have documentation for every tech stack on Earth, right now we're not there. If you feel that we're missing something that should be here, here are a few options:
+
+## Generating pkd-files
+
+The fastest way to get access to the docs you need is by using the [PikaDoc CLI](https://github.com/ArseAssassin/pikadoc). In addition to the docs available in the central repository, this tool can generate documentation from many popular sources such as npm, pypi, GitHub repos, and more. Getting docs from a featured source is as simple as typing:
+
+> \`doc src:{source} use {package}\`
+
+In addition to the basic features that this browser client supports, the CLI supports features advanced queries, looking up symbol source code and more.
+
+## Featuring docs on the central repository
+
+If you feel there's something that we should add to the repository, you can raise an issue on our [issue tracker](https://github.com/ArseAssassin/pkdocs/issues). The best way to have your project featured is to add a \`DOCS.pkd\` file following the \`.pkd\` YAML file format to the root of your repo. For more information on the file format, see the documentation in the pikadoc repo.
+      `}</Markdown>
+    </div>
+  </div></div>
+}
+
+function FrontPage() {
+  return <div></div>
+}
+
+function NotFound() {
+  return <div className="error-page">
+    <h1>404</h1>
+    <p>Looks like there's nothing here</p>
   </div>
 }
 
@@ -292,20 +424,21 @@ function App() {
         <a href='https://github.com/ArseAssassin/pikadoc'>pkDocs on GitHub</a>
       </nav>
       <Router hook={ useHashLocation }>
-        <Route path='/:gen?/:doc?/:symbol?'>
-          {(params) =>
-            <DoctableSearch
-              selectedDoc={
-                params.gen && params.doc
-                  ? params.gen + '/' + params.doc
-                  : undefined
-              } />}
-        </Route>
         <Switch>
-          <Route path='/' component={ FrontPage } />
+          <Route path='/something-missing' component={ SomethingMissing } />
+          <Route path='/' component={ DoctableSearch } />
           <Route path='/:gen/:doc/:symbol?'>
-            {(params) => <DocPage doc={ params.gen + '/' + params.doc } symbolId={ params.symbol } />}
+            {(params) => <>
+              <DoctableSearch
+                key='search-bar'
+                selectedDoc={ params.gen + '/' + decodeURIComponent(params.doc) } />
+              <DocPage
+                gen={ params.gen }
+                doc={ decodeURIComponent(params.doc) }
+                symbolId={ params.symbol } />
+            </>}
           </Route>
+          <Route><NotFound /></Route>
         </Switch>
       </Router>
     </div>
